f2bc5c1bc41faf21c2457b9e23faf4c089497caa
ryom <ryotagranpa@gmail.com>
2021-11-26 12:07:08 +0900

    SectorMapとりあえず完成

---
+++ Assets/Scripts/Configs/StepGenerationConfig.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class StepGenerationConfig:SingleScriptableObject<StepGenerationConfig>
+{
+    [SerializeField] float maxMiasma;
+    [SerializeField] Vector2 mapSize;
+    [SerializeField]  
+
+    [SerializeField] 
+    public SectorStep GenerateStep(Vector2 coordinate)
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Scripts/SalvageSession/Sector/SectorMap.cs
+++ Assets/Scripts/SalvageSession/Sector/SectorMap.cs
@@ -7,14 +7,23 @@ public class SectorMap
     Dictionary<Vector2Int, SectorStep> _mapData;
     public Dictionary<Vector2Int, SectorStep> mapData { get { return _mapData; } }
 
-    public bool SetIsland()
+    public bool TryAddStep(Vector2Int coordinate, SectorStep step)
     {
-        
+        if(!CheckCoordinate(coordinate,step.radius))
+        {
+            _mapData[coordinate] = step;
+        }
+        return false;
     }
 
 
-    //座標を探る。Rangeを入れると、その範囲内にあるシマの中で一番座標に近いものを返す
-    public SectorStep CheckCoordinate(Vector2Int coordinate, float range = 0)
+    /// <summary>
+    /// 座標と範囲円の半径でその中に島があるかを確認する
+    /// </summary>
+    /// <param name="coordinate"></param>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    public bool CheckCoordinate(Vector2Int coordinate, float range = 0)
     {
         float sqrDistance;
         foreach (var stepCoordPair in _mapData)
@@ -22,22 +31,23 @@ public class SectorMap
             sqrDistance = Vector2.Distance(stepCoordPair.Key, coordinate);
             if (sqrDistance < range)
             {
-                return stepCoordPair.Value;
+                return true;
             }
         }
 
-        return null;
+        return false;
     }
 
+
     /// <summary>
-    /// 範囲(円)内にあるすべてのシマを返す
+    /// 範囲円の中にあるすべてのシマを取得する
     /// </summary>
     /// <param name="coordinate">中心の座標</param>
     /// <param name="range">範囲円の半径</param>
     /// <param name="result">結果を入れるやつ</param>
     /// <param name="refreshResult">結果を都度Clearするか</param>
     /// <returns></returns>
-    public bool CheckRange(Vector2Int coordinate, float range, ref List<SectorStep> result, bool refreshResult = false)
+    public bool TryFindRange(Vector2Int coordinate, float range, ref List<SectorStep> result, bool refreshResult = false)
     {
         float sqrDistance;
         bool ifFound = false;
