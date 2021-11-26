ccd63b934e99987b6cee4a09c5a5682ef2a45d86
ryom <ryotagranpa@gmail.com>
2021-11-26 16:57:21 +0900

    GameManager若干成型

--- Assets/Scripts/EventSystem/GameManager.cs
+++ Assets/Scripts/EventSystem/GameManager.cs
@@ -11,38 +11,15 @@ public class GameManager : Singleton<GameManager>
         StartCoroutine(SystemInitialize());
     }
 
-    /*
-    public void RegistInteraptor(SmallTask task)
-    {
-        interaptorQueue.RegisterQueue(task);
-    }
-    */
-
     IEnumerator SystemInitialize()
     {
         yield return 4;
 
-        /*
-        //あぁ～これはいい実装では？
-        foreach (var state in sysEvents)
-        {
-            var events = OnSystemEvent.GetInvocationList();
-
-            for(int i =0;i < events.Length;i++)
-            {
-                var ev = events[i] as Func<GameState,SmallTask>;
-                var task = ev(state);
-                interaptorQueue.RegisterQueue(task);
-            }
-            yield return StartCoroutine(HandleInteraptor());
-        }
-        */
-        
         var task = EventManager.instance.Notice(EventName.SystemEvent,new SystemEventArg(GameState.SystemInitialize));
         yield return new WaitUntil(()=>task.compleated);
 
-       task = EventManager.instance.Notice(EventName.SystemEvent,new SystemEventArg(GameState.ViewInitialize));
-       yield return new WaitUntil(()=>task.compleated);
+        task = EventManager.instance.Notice(EventName.SystemEvent,new SystemEventArg(GameState.ViewInitialize));
+        yield return new WaitUntil(()=>task.compleated);
     }
 
 }
